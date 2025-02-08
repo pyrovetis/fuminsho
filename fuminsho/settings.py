2d1963ddf9dd7ce80b027e0d5b404bf38d302b46
Sobhan <46265207+pyrovetis@users.noreply.github.com>
2025-02-08 19:54:02 +0330

    Update settings.py

--- fuminsho/settings.py
+++ fuminsho/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
