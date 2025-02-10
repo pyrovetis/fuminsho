5167dc064397fcb9acc48d7ee74f25667f5f6e0c
Sobhan <46265207+pyrovetis@users.noreply.github.com>
2025-02-10 13:05:25 +0330

    add canonical link

--- core/context.py
+++ core/context.py
@@ -1,3 +1,4 @@
 def main(request):
     path = request.path
-    return {"path": path}
+    canonical_path = request.build_absolute_uri(path)
+    return {"path": path, "canonical_path": canonical_path}
